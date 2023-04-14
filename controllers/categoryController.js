0d84c4c03216dec39b34f21d2d9a037287b66f99
Fred-18 <fred.nobre@octo.com>
2023-04-14 10:00:46 +0200

    :sparkles: add new category contollers

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -1,23 +1,53 @@
-import Category from "../models/Category.js";
+import categoryModel from "../models/categoryModel.js";
 
 // Get All Categories
-export const getCategories = (req, res, next) => {
-    res.render("category/getCategories", {
-        title: "CategoryList",
-    });
+export const getCategories = async (req, res, next) => {
+    const category = await categoryModel.find()
+    res.status(200).render("category/getCategories", {title: "CategoryList", category});
 };
 
-// Create Category
+// Create CategoryModel||
 export const postCategory = async (req, res, next) => {
     // Send categoryName to the req.body
     const categoryName = req.body.categoryName;
+    const categoryDescription = req.body.categoryDescription
 
     // create new category with mongoose
-    const category = await Category.create({
-        categoryName,
+    const category = await categoryModel.create({
+        categoryName,categoryDescription
     });
 
     console.log(category);
     //   res.status(201).redirect("/categories");
-    res.status(201).json({ category });
+    res.status(201).json({category});
 };
+
+//Delete category
+export const deleteCategoryById = async (req, res, next) => {
+    // Send categoryName to the req.body
+    const categoryId = req.body._id;
+
+
+    // create new category with mongoose
+    const category = await categoryModel.findByIdAndDelete({_id: categoryId});
+
+    //   res.status(201).redirect("/categories");
+    res.status(201).json({category});
+};
+
+//update category
+export const updateCategoryById = async (req, res, next) => {
+    // Send categoryName to the req.body
+    const _id = req.body._id;
+    const categoryName = req.body.categoryName;
+    const categoryDescription = req.body.categoryDescription;
+    
+
+    // create new category with mongoose
+    const category = await categoryModel.findByIdAndUpdate(_id, {categoryName,categoryDescription});
+
+    //   res.status(201).redirect("/categories");
+    res.status(201).json({category});
+};
+
+
