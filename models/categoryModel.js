ca380c623e483af0124d53c14e1c9309648d2316
Fred-18 <fred.nobre@octo.com>
2023-04-14 10:00:08 +0200

    :sparkles: add category description

---
+++ models/categoryModel.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+const { Schema } = mongoose;
+
+const categorySchema = new Schema({
+    categoryName: {
+        type: String,
+        required: true,
+    },
+    
+    categoryDescription: {
+        type: String,
+        required: true,
+    }
+});
+
+const CategoryModel = mongoose.model("category", categorySchema);
+
+export default CategoryModel;
