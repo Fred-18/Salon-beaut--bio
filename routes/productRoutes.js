53f2b47bc56993dae632b0be3f496af69b6d700b
Fred-18 <fred.nobre@octo.com>
2023-05-01 18:35:40 +0200

    :art: update productRoutes

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -2,18 +2,23 @@ import express from "express";
 import {
     getProducts,
     postProduct,
+    getProductById,
     deleteproductById,
-    updateproductById
-} from "../controllers/productController.js";
+    updateproductById,
+    getProductByCategotyId} from "../controllers/productController.js";
 
 const router = express.Router();
 
 router.get("/products", getProducts);
 
+router.get("/product",getProductById)
+
 router.post("/create-product", postProduct);
 
-router.post("/delete-product", deleteproductById);
+router.post("/delete-product/", deleteproductById);
 
 router.post("/update-product", updateproductById);
+
+router.get("/categorie-pages/:ownedbyCategory",getProductByCategotyId);
 
 export default router;
