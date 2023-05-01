1d8d3067fb4d6d6042f1a916acdd8f1a8d55534f
Fred-18 <fred.nobre@octo.com>
2023-05-01 18:22:08 +0200

    :sparkles: add  categoryModel.js

--- models/categoryModel.js
+++ models/categoryModel.js
@@ -1,16 +1,18 @@
 import mongoose from "mongoose";
-const { Schema } = mongoose;
+
+const {Schema} = mongoose;
 
 const categorySchema = new Schema({
     categoryName: {
         type: String,
         required: true,
     },
-    
+
     categoryDescription: {
         type: String,
         required: true,
     }
+    
 });
 
 const CategoryModel = mongoose.model("category", categorySchema);
